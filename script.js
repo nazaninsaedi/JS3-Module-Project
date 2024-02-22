177798c2395aa8e91c8effb7daa2e12d0306b576
Nazanin Saedi <niliom1988@gmail.com>
2024-02-22 22:09:00 +0000

    Nazanin Level 200 Done

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
+// function setup() {
+//   const allEpisodes = getAllEpisodes();
+//   makePageForEpisodes(allEpisodes);
+// }
+
+// function makePageForEpisodes(episodeList) {
+//   const rootElem = document.getElementById("root");
+//   rootElem.textContent = ""; // Clear existing content
+
+//   episodeList.forEach((episode) => {
+//     const episodeDiv = document.createElement("div");
+//     episodeDiv.classList.add("episode");
+
+//     const episodeTitle = document.createElement("h2");
+//     const episodeCode = `S${episode.season
+//       .toString()
+//       .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+//     episodeTitle.textContent = `${episode.name} - ${episodeCode}`;
+
+//     const seasonNumber = document.createElement("p");
+//     seasonNumber.textContent = `Season: ${episode.season}`;
+
+//     const episodeNumber = document.createElement("p");
+//     episodeNumber.textContent = `Episode: ${episode.number}`;
+
+//     const episodeImage = document.createElement("img");
+//     episodeImage.src = episode.image.medium;
+//     episodeImage.alt = episode.name;
+
+//     const episodeSummary = document.createElement("p");
+//     episodeSummary.innerHTML = episode.summary;
+
+//     episodeDiv.appendChild(episodeTitle);
+//     episodeDiv.appendChild(seasonNumber);
+//     episodeDiv.appendChild(episodeNumber);
+//     episodeDiv.appendChild(episodeImage);
+//     episodeDiv.appendChild(episodeSummary);
+
+//     rootElem.appendChild(episodeDiv);
+//   });
+
+//   // Add attribution to TVMaze
+//   const attribution = document.createElement("p");
+//   attribution.innerHTML =
+//     'Data sourced from <a href="https://tvmaze.com/" target="_blank">TVMaze.com</a>';
+//   rootElem.appendChild(attribution);
+// }
+
+// window.onload = setup;
 function setup() {
   const allEpisodes = getAllEpisodes();
+  state.allEpisodes = allEpisodes;
   makePageForEpisodes(allEpisodes);
 }
 
@@ -12,23 +62,20 @@ function makePageForEpisodes(episodeList) {
     episodeDiv.classList.add("episode");
 
     const episodeTitle = document.createElement("h2");
-    const episodeCode = `S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
+    const episodeCode = `S${episode.season.toString().padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
     episodeTitle.textContent = `${episode.name} - ${episodeCode}`;
-
     const seasonNumber = document.createElement("p");
-    seasonNumber.textContent = `Season: ${episode.season}`;
-
     const episodeNumber = document.createElement("p");
-    episodeNumber.textContent = `Episode: ${episode.number}`;
-
     const episodeImage = document.createElement("img");
     episodeImage.src = episode.image.medium;
     episodeImage.alt = episode.name;
-
     const episodeSummary = document.createElement("p");
     episodeSummary.innerHTML = episode.summary;
+    const episodeOption = document.createElement("option");
+    episodeOption.value = episode.id;
+    episodeOption.text = `${episodeCode} - ${episode.name}`;
+    dropDownMenu.appendChild(episodeOption);
+    episodeDiv.id = `episode - ${episode.id}`;
 
     episodeDiv.appendChild(episodeTitle);
     episodeDiv.appendChild(seasonNumber);
@@ -44,6 +91,54 @@ function makePageForEpisodes(episodeList) {
   attribution.innerHTML =
     'Data sourced from <a href="https://tvmaze.com/" target="_blank">TVMaze.com</a>';
   rootElem.appendChild(attribution);
+}
+
+// level 200 
+
+const state = {
+  allEpisodes: [],
+  searchTerm: ""
+};
+
+function render() {
+  const filteredEpisodes = state.allEpisodes.filter(function (episode) {
+    return episode.name.toLowerCase().includes(state.searchTerm.toLowerCase()) || episode.summary.toLowerCase().includes(state.searchTerm.toLowerCase());
+  });
+
+  makePageForEpisodes(filteredEpisodes);
+
+  document.getElementById("search-info").textContent = `Displaying ${filteredEpisodes.length} / 73 episodes`;
+};
+
+const input = document.querySelector("#q");
+
+input.addEventListener("input", function () {
+  state.searchTerm = input.value.toLowerCase();
+  render();
+});
+
+const dropDownMenu = document.getElementById("select");
+dropDownMenu.addEventListener("change", function () {
+  const selectedValue = dropDownMenu.value;
+
+  if (selectedValue === "all") {
+    render(state.allEpisodes);
+  }
+  else {
+    const selectedEpisode = state.allEpisodes.find((episode) => episode.id === parseInt(selectedValue));
+    render([selectedEpisode]);
+    navigateToEpisode(selectedEpisode);
+  }
+});
+
+function navigateToEpisode(episode) {
+  if (episode) {
+    const episodeElement = document.getElementById(`episode - ${episode.id}`);
+
+    if (episodeElement) {
+      episodeElement.scrollIntoView({behavior: "smooth"});
+    }
+  }
 }
 
 window.onload = setup;
